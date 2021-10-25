38196b9fd2d58b7f001a2961013db00c2b68eeaf
pakaredukasi <>
2021-10-25 09:17:57 +0700

    add admin template

---
+++ resources/views/table.blade.php
@@ -0,0 +1,4 @@
+@extends('app.master')
+@section('content')
+    arsyad belajar laravel
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/master', function () {
+    return view('app.master');
+});
+
+Route::get('/table', function () {
+    return view('table');
+});
